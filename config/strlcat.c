c0ae88ee69653f6d20cf2bff3e7175eed29fa43e
Sebastian <versat@users.noreply.github.com>
2022-03-20 14:45:36 +0100

    config/strlcat.c: Fix constant for array size

--- config/strlcat.c
+++ config/strlcat.c
@@ -1,11 +1,12 @@
 #include <string.h>
 
+#define BUFFER_SIZE 16
+
 int main(int argc, char **argv) {
 	int retval;
-	int size = 16;
-	char buffer[size] = {0};
+	char buffer[BUFFER_SIZE] = {0};
 
-	retval = strlcat(buffer, "hello", size);
+	retval = strlcat(buffer, "hello", BUFFER_SIZE);
 
 	return !!retval;
 }
